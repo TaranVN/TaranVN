1f33a561a7957d1b34e18c30ed763a3b90a5b555
TaranVN <90509686+TaranVN@users.noreply.github.com>
2021-10-28 16:17:10 +0300

    Add files via upload

---
+++ Занятие 3/3.4.py
@@ -0,0 +1,7 @@
+# -- coding: utf-8 --
+N=int(input('Количество чисел '))
+A=0
+for i in range (N):
+    X=int(input())
+    A=A+X
+print(A)
---
+++ Занятие 3/3.5.py
@@ -0,0 +1,6 @@
+# -- coding: utf-8 --
+n=int(input())
+A=0
+for i in range (n):
+    A=A+(i**3)
+print(A)
