53b42020bfe23dbff6db0c7097a4e2b95d3d6514
TaranVN <90509686+TaranVN@users.noreply.github.com>
2022-04-15 16:01:15 +0300

    Add files via upload

---
+++ Занятие Java 1/Z2.java
@@ -0,0 +1,15 @@
+
+public class Z2 {
+public static void main(String par[])
+{
+int N=par.length;
+if(N>0)
+{
+System.out.println("Вы ввели "+ N +" параметров");
+}
+else
+{
+System.out.println("Вы не передавали параметров");
+}
+}
+}
---
+++ Занятие Java 1/Z4.java
@@ -0,0 +1,16 @@
+
+public class Z4 {
+public static void main(String N[])
+{
+String login="Taran";
+String pass="qwerty";
+if((N[0].equals(login))&(N[1].equals(pass)))
+{
+System.out.println("Вас узнали. Добро пожаловать.");
+}
+else
+{
+System.out.println("Логин и пароль не распознаны. Доступ запрещён.");
+}
+}
+}
