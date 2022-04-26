fef720dc49ce7890554e98ce8004c0f0c8a9dee1
TaranVN <90509686+TaranVN@users.noreply.github.com>
2022-04-26 15:25:53 +0300

    Update Z3.java

--- Занятие Java 1/Z3.java
+++ Занятие Java 1/Z3.java
@@ -1,16 +1,14 @@
-
 public class Z3 {
-public static void main(String N[])
-{
-if(N.length==2)
-{
-int x = Integer.parseInt(N[0]);
-int y = Integer.parseInt(N[1]);
-System.out.println(N[0]+" + "+N[1]+" = "+(x+y));
+	public static void main(String N[])
+	{
+		if(N.length==2) {
+			int x = Integer.parseInt(N[0]);
+			int y = Integer.parseInt(N[1]);
+			System.out.println(N[0]+" + "+N[1]+" = "+(x+y));
+		}
+		else {
+			System.out.println("РќРµРІРµСЂРЅРѕРµ РєРѕР»РёС‡РµСЃС‚РІРѕ РїР°СЂР°РјРµС‚СЂРѕРІ");
+		}
+	}
 }
-else
-{
-System.out.println("Неверное количество параметров");
-}
-}
-}
+
