84a3576e99b683abe7e494f295ea6afa44298f2c
TaranVN <90509686+TaranVN@users.noreply.github.com>
2022-04-30 19:07:03 +0300

    Add files via upload

---
+++ Занятие Java 2/Z21.java
@@ -0,0 +1,19 @@
+public class Z21 {
+	public static void main(String name[]) {
+		int array[] = new int[31];
+		for (int i = 0; i < 31; i++)
+		 {
+		array[i] = -100 + (int)Math.round(Math.random()*
+		(100 - -100));
+	     }
+		for (int i = 0; i < 31; i++) {
+			System.out.print(i + " - й элемент массива = " + array[i] + "\n"); }
+		int count = 0;
+		for (int i = 1; i < 30; i++) {
+			if((array[i] < 0 && array[i + 1] >= 0) || (array[i] >= 0 && array[i + 1] < 0)) {
+				count++; 
+			}
+		}
+		System.out.print("Знак элементов меняется " + count + " раз");
+	}
+}
