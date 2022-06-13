95aa22d02de6c7f29d20feaf52f4929a01fc42f7
TaranVN <90509686+TaranVN@users.noreply.github.com>
2022-06-13 23:47:11 +0300

    Update Z31.java

--- Занятие Java 3/Z31.java
+++ Занятие Java 3/Z31.java
@@ -1,12 +1,12 @@
 public class Z31 {
 	public static void main(String[] args) {
 		Car car = new Car("Mercedes-Benz AMG III");
-		car.DataMade(2015);
-		car.Price(5050000);
-		car.Configuration("S63 AMG");
-		car.MadeIn("Germany");
-		car.Sold("06.06.2022");
-		car.Buyer("John Smith");
+		car.setDataMade(2015);
+		car.setPrice(5050000);
+		car.setConfiguration("S63 AMG");
+		car.setMadeIn("Germany");
+		car.setSold("06.06.2022");
+		car.setBuyer("John Smith");
 		
 		System.out.println(car.toString());
 	}
